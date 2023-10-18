e8b06ca7b75206afae048383deda99d867a1be1a
MatisOana6 <126575284+MatisOana6@users.noreply.github.com>
2023-10-18 11:47:50 +0300

    commit2

--- ghostAgents.py
+++ ghostAgents.py
@@ -19,28 +19,36 @@ import random
 from util import manhattanDistance
 import util
 
-class GhostAgent( Agent ): 
+class GhostAgent( Agent ): # Define a new class named GhostAgent that inherits from the Agent class.
+    # Define the constructor method for GhostAgent
     def __init__( self, index ):
+        # Inside the constructor, assign the 'index' parameter passed to the constructor
+        # to an instance variable named 'index'. This allows each GhostAgent instance
+        # to store its own 'index' value.
         self.index = index
 
-    def getAction( self, state ):
-        dist = self.getDistribution(state)
+    def getAction( self, state ): # Define a method with two parameters 'self' (referring to the instance) and 'state'. 
+        dist = self.getDistribution(state) # Call the 'getDistribution' method of the current instance (self) with 
+        #the 'state' parameter,and store the result in a variable named 'dist'
         if len(dist) == 0:
             return Directions.STOP
         else:
-            return util.chooseFromDistribution( dist )
+            return util.chooseFromDistribution( dist ) 
+        # If the distribution is not empty, use the 'util.chooseFromDistribution' function 
+        # to select an action based on the given distribution, and return that action.
 
     def getDistribution(self, state):
         "Returns a Counter encoding a distribution over actions from the provided state."
-        util.raiseNotDefined()
+        util.raiseNotDefined() # It raises a "NotImplementedError" exception 
 
 class RandomGhost( GhostAgent ):
     "A ghost that chooses a legal action uniformly at random."
     def getDistribution( self, state ):
-        dist = util.Counter()
-        for a in state.getLegalActions( self.index ): dist[a] = 1.0
-        dist.normalize()
-        return dist
+        dist = util.Counter() # Create an empty Counter object to store the action distribution.
+        for a in state.getLegalActions( self.index ): dist[a] = 1.0 # Loop through all legal actions available to the ghost in the given 'state'.
+        # Set the probability of taking the action 'a' to 1.0 in the 'dist' Counter.
+        dist.normalize()  # Normalize the distribution so that the probabilities add up to 1.0.
+        return dist # Returns the normalized distribution of actions
 
 class DirectionalGhost( GhostAgent ):
     "A ghost that prefers to rush Pacman, or flee when scared."
@@ -59,13 +67,17 @@ class DirectionalGhost( GhostAgent ):
         speed = 1
         if isScared: speed = 0.5
 
+        # Create a list of vectors representing the movement direction for each legal action at a given 'speed'.
         actionVectors = [Actions.directionToVector( a, speed ) for a in legalActions]
+        # Create a list of new positions by adding the direction vectors to the current position.
         newPositions = [( pos[0]+a[0], pos[1]+a[1] ) for a in actionVectors]
+        # Get the current position of the Pacman agent from the 'state' object.
         pacmanPosition = state.getPacmanPosition()
 
         # Select best actions given the state
+        # Calculate the Manhattan distances from each new position to the Pacman's current position.
         distancesToPacman = [manhattanDistance( pos, pacmanPosition ) for pos in newPositions]
-        if isScared:
+        if isScared: # Check if the ghost is in a "scared" state (when it's fleeing from Pacman).
             bestScore = max( distancesToPacman )
             bestProb = self.prob_scaredFlee
         else:
